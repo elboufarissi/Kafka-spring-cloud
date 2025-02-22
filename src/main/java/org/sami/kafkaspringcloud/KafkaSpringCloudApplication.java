a5d9d224d5d1fdced237d290593a8441424dafb9
elboufarissi (course) <elboufarissisami@gmail.com>
2025-02-22 03:03:03 +0100

    Initial commit

---
+++ src/main/java/org/sami/kafkaspringcloud/KafkaSpringCloudApplication.java
@@ -0,0 +1,13 @@
+package org.sami.kafkaspringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpringCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSpringCloudApplication.class, args);
+    }
+
+}
